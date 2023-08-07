e5680e493231c0b8b9e4f6c255e81cf77ed4bf84
Meek Msaki <meek10x@gmail.com>
2023-08-07 03:58:17 -0500

    fix: huff contracts moved

---
+++ contracts/3.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract Level_3_Solution {
+    /// ### Calldata layout
+    /// 
+    /// `selector . value`
+    ///  
+    /// | name     | size (bytes) | offset |
+    /// | -------- | ------------ | ------ |
+    /// | selector | 4            | 0x00   |
+    /// | size     | 32           | 0x04   |
+    /// | length   | 32           | 0x24   |
+    /// | value    | 32           | 0x44   |
+    function solution(bytes memory packed) external pure returns (uint8 a, bool b, bytes1 c) {
+        assembly {
+            a := shr(mul(0x08, 0x1f), calldataload(0x44))
+            b := and(shr(mul(0x08, 0x1e), calldataload(0x44)), 0x00ff)
+            c := shl(mul(0x08, 0x1f), and(shr(mul(0x08, 0x1d), calldataload(0x44)), 0x0000ff))
+        }
+    }
+}
